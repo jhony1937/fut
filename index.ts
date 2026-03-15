ea6ac34982eeda348b5d707141b5bf635d1f4ad6
jhony1937 <aleexaleex259@gmail.com>
2026-03-15 03:32:42 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ export let room: RoomObject;
 HaxballJS.then(async (HBInit) => {
   await initDatabase();
   room = HBInit({
-    roomName: "🟨 𝑴𝑫𝑿 | 𝑭𝑼𝑻𝑺𝑨𝑳 3v3 | Ranked  🟨",
+    roomName: "🟨 MDX | Futsal 3v3 | Ranked 🟨",
     maxPlayers: 20,
     public: true,
     noPlayer: true,
