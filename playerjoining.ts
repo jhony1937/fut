877e7c2680c4dbd90861a4e43ec0f6d514b63f3d
jhony1937 <aleexaleex259@gmail.com>
2026-03-19 03:55:55 +0000

    Update playerjoining.ts

--- playerjoining.ts
+++ playerjoining.ts
@@ -1,4 +1,4 @@
-import { room, specPlayerIdList, debuggingMode, playerConnStrings, adminAuthList, redPlayerIdList, restartGameWithCallback, bluePlayerIdList } from "./index.js";
+import { room, specPlayerIdList, debuggingMode, adminAuthList, redPlayerIdList, restartGameWithCallback, bluePlayerIdList } from "./index.js";
 import { checkAndHandleBadWords } from "./moderation.js";
 import { movePlayerToTeam, moveOneSpecToEachTeam } from "./teammanagement.js";
 import { getPlayerStatsFromDB } from "./stats.js";
@@ -8,7 +8,6 @@ export async function handlePlayerJoining(player: PlayerObject): Promise<void> {
     const playerName: string = player.name;
     const playerList: PlayerObject[] = room.getPlayerList();
     if (checkAndHandleBadWords(player, playerName)) return;
-    if (isPlayerAlreadyConnected(player, player.conn)) return;
     if (adminAuthList.has(player.auth)) room.setPlayerAdmin(playerId, true);
     
     // Load player stats into cache for synchronous access (e.g. chat)
@@ -31,15 +30,4 @@ function checkAndRestartWithNewMode(playerList: PlayerObject[]): void {
     if (playerListLength === 1) restartGameWithCallback(() => movePlayerToTeam(playerList[0]!.id, redPlayerIdList));
     if (playerListLength === 2) restartGameWithCallback(() => movePlayerToTeam(specPlayerIdList[0]!, bluePlayerIdList));
     if (playerListLength <= 6 && specPlayerIdList.length === 2) restartGameWithCallback(() => moveOneSpecToEachTeam());
-}
-
-function isPlayerAlreadyConnected(player: PlayerObject, conn: string): boolean {
-    const playerId = player.id;
-    if (!debuggingMode && [...playerConnStrings.values()].some(value => value === conn)) {
-        room.kickPlayer(playerId, "You are already connected to the room", false);
-        console.warn(`>>> ${player.name} was kicked. Reason: connecting twice with the same IP.`);
-        return true;
-    }
-    playerConnStrings.set(playerId, conn);
-    return false;
 }
