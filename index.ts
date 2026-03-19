9235b3240c36eda19aae80c1b1962362d6e9972c
jhony1937 <aleexaleex259@gmail.com>
2026-03-19 04:15:23 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -42,9 +42,9 @@ HaxballJS.then(async (HBInit) => {
     }
   });
   room = HBInit({
-    roomName: "Testing",
+    roomName: "Futsal|3v3|Testing|v Beta",
     maxPlayers: 20,
-    public: false,
+    public: true,
     noPlayer: true,
     geo: {
       code: "MA",
